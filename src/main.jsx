8f20e132a6b44cc75b49e4d2aa87753aef3e62d7
quanganh261299 <quanganhdoan5599@gmail.com>
2023-09-12 23:45:08 +0700

    update auth

--- src/main.jsx
+++ src/main.jsx
@@ -5,32 +5,43 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
-import ErrorPage from "./routes/error-page";
-import Contact, {loader as contactLoader} from "./routes/contact";
-import EditContact from "./routes/edit";
-import {action as editAction} from "./routes/edit";
-import { action as destroyAction } from "./routes/destroy";
-import Index from "./routes/index";
+import Root, {
+  loader as rootLoader, action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
-//data res
-import DataRes from "./components/routes/dataRes";
-import DataEditComponent from "./components/routes/dataEdit";
+import { action as editAction } from "./routes/edit"
+
+import EditContact from "./routes/edit";
+
+import { action as destroyAction } from './routes/destroy';
+import Index from "./routes";
+
+// data res
+import DataRes from "./component/routes/dataRes";
+import DataEditComponent from "./component/routes/dataEdit";
+
+import RegisterAndLogin from './auth/RegisterAndLogin';
+import RequireAuth from './auth/RequireAuth';
+import { fakeAuthProvider } from './auth/auth';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:  <Root />,
+    element: <RequireAuth>
+      <Root />
+    </RequireAuth>,
     errorElement: <ErrorPage />,
-    // call và load data từ contacts.js
+    // call và load data từ bên contact
     loader: rootLoader,
     action: rootAction,
-    children:[
+    children: [
       { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
-        loader: contactLoader
+        loader: contactLoader,
       },
       {
         path: "contacts/:contactId/edit",
@@ -43,19 +54,59 @@ const router = createBrowserRouter([
         action: destroyAction,
       },
       {
-        path:'data',
+        path: "data",
         element: <DataRes />
       },
       {
-        path:'data/dataDetail/:dataId',
+        path: "data/dataDeatail/:dataId",
         element: <DataEditComponent />
       }
     ],
+
   },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
+  {
+    path: "/login",
+    element: <RegisterAndLogin />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
+
+
+let AuthContext = React.createContext(null);
+
+export const useAuth = () => {
+  return React.useContext(AuthContext);
+}
+
+function AuthProvider({ children }) {
+  let [user, setUser] = React.useState(null);
+
+  let signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback;
+    });
+  };
+
+  let signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback;
+    });
+  };
+
+  let value = { user, signin, signout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
